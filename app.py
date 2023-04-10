7bd68ab9d432357131ab823fc315a9d939b6cbca
Hank <balimon@hotmail.com>
2023-04-10 20:31:43 +0800

    'ok'

--- app.py
+++ app.py
@@ -53,9 +53,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    GPT_answer = GPT_response(msg)
-    print(GPT_answer)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    # GPT_answer = GPT_response(msg)
+    # print(GPT_answer)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
