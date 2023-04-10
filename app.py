3a5276c675799c16f46b4fe8295baa51d3d94a04
Hank <balimon@hotmail.com>
2023-04-10 22:31:29 +0800

    'exchange2davinci003'

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", prompt=text, temperature=0.7, max_tokens=500)
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('\n','').replace('。','')
