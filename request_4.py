f83a010b6647aca63956e52eb7dd27e7ddbf7ece
Watermelon-1234 <948794harvey@gmail.com>
2023-08-09 16:04:26 +0800

    test

--- request_4.py
+++ request_4.py
@@ -1,14 +1,16 @@
 from icrawler.downloader import ImageDownloader
 from icrawler.builtin import GoogleImageCrawler
+from icrawler.utils import Session
+
 
 class CustomLinkPrinter(ImageDownloader):
     file_urls = []
-
     def get_filename(self, task, default_ext):
         file_idx = self.fetched_num + self.file_idx_offset
         return '{:04d}.{}'.format(file_idx, default_ext)
 
     def download(self, task, default_ext, timeout=5, max_retry=3, overwrite=False, **kwargs):
+        self.session.headers['Cache-Control'] = 'no-store'
         file_url = task['file_url']
         filename = self.get_filename(task, default_ext)
 
@@ -25,19 +27,24 @@ class CustomLinkPrinter(ImageDownloader):
 
         return
 
+
 init_params = {
     'downloader_cls': CustomLinkPrinter,
     # 添加其他参数
 }
-def get_img_url(keyword):
-    google_crawler = GoogleImageCrawler(**init_params)
+def get_img_url(keyword="yee"):
+    
+    google_crawler = GoogleImageCrawler( **init_params)
     google_crawler.crawl(keyword=keyword+" 梗圖", max_num=1)  # 根据需要调整参数
 
     file_urls = google_crawler.downloader.file_urls
     rtn=""
     if(file_urls[0]):
-        rtn = file_urls[0]
+        if(file_urls[0].count('http')>1):
+           file_urls[0] = "http"+file_urls[0].rsplit('http', 1)[-1]
+        rtn =file_urls[0]
     else:
         rtn = "https://memeprod.ap-south-1.linodeobjects.com/user-template/0f3ce1930440d817e8a477a175f871ed.png"
     print("result",rtn)
     return rtn
+#get_img_url()
