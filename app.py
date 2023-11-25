1977b2e8068f7c35362e8ed26fad55974a74e5ec
maso0310 <36530475+maso0310@users.noreply.github.com>
2023-11-25 22:44:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import tempfile, os
 import datetime
 import openai
 import time
+import traceback
 #======python的函數庫==========
 
 app = Flask(__name__)
@@ -59,6 +60,7 @@ def handle_message(event):
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
+        print(traceback.exec_format())
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
         
 
