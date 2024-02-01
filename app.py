a9a13a3b7e79f9f56ff66e289c46621a7df40967
maso0310 <36530475+maso0310@users.noreply.github.com>
2024-02-01 12:23:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
